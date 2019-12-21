f38738c31af56dcca2489bb162a01ddad8317a1d
SoniArpit <arpitsoni362@gmail.com>
2019-12-21 16:59:02 +0530

    first program

--- HelloJava.java
+++ HelloJava.java
@@ -1,7 +1,5 @@
 class HelloJava{
     public static void main(String args[]){
         System.out.println("Hello Java!! I'm Back");
-        System.out.println("Happy to learn Java");
-        System.out.println("Happy to learn Java");
     }
 }
